9e3a9d2a0e67afe586da935ce02cd4cc2128b405
dreais <valentin.lattay@gmail.com>
2019-11-21 18:33:29 +0100

    populated main file

---
+++ Sources/main.c
@@ -0,0 +1,14 @@
+#include "../Headers/core_ftp.h"
+
+void copy_file()
+{
+
+}
+
+int main(void)
+{
+    get_log_file();
+    output_logs_str(PREFIX_INFO, "Welcome to FTP.\n");
+    close_file();
+    return 0;
+}

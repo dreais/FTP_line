5a2ca836e091d2b1ffc4bed14872824cd2075f1e
dreais <valentin.lattay@gmail.com>
2020-05-24 20:45:41 +0200

    header

--- Headers/core_ftp.h
+++ Headers/core_ftp.h
@@ -18,11 +18,17 @@ struct raw_com {
     void (*func)(char *);
 };
 
+struct pollfds {
+    struct pollfd s[2];
+    int ret_v[2];
+};
+
 /**			**\
  * 	TYPEDEFs	 *
 \**			**/
 typedef unsigned char byte; // 1 byte
 typedef struct raw_com raw_com_t;
+typedef struct pollfds poll_t;
 
 /**			**\
  *	PROTOTYPEs	 *
@@ -30,9 +36,9 @@ typedef struct raw_com raw_com_t;
 void handle_req(char command[5], char *args);
 // Sockets/
 void destroy_socket(int fd);
-void receive_connections(struct pollfd *sockets, int *ret_v);
+void receive_connections(poll_t *pollfds, int size);
 int create_socket(char *ip_addr, short port);
 
-int send_command(struct pollfd sockets[2], int index, char *CMD, char *args);
+int send_command(poll_t *pollfds, int index, char *CMD, char *args);
 
 #endif //CORE_FTP_H
